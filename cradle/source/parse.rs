dca1454971ee56b8c2860edf0aa426cd099ea91f
SirGolem <sirgolemofficial@gmail.com>
2026-09-14 17:05:55 +0100

    fix: add parsing function return values

--- cradle/source/parse.rs
+++ cradle/source/parse.rs
@@ -4,18 +4,24 @@ use crate::{
     state::State,
 };
 
-pub fn parse_identifier(state: &mut State) -> () {
+pub fn parse_identifier(state: &mut State) -> char {
     match state.character {
         | None => expected("an identifier", "end of file"),
-        | Some(character) if is_alphabetic(character) => get_character(state),
+        | Some(character) if is_alphabetic(character) => {
+            get_character(state);
+            return character;
+        }
         | Some(character) => expected("an identifier", format!("'{character}'")),
     };
 }
 
-pub fn parse_integer_literal(state: &mut State) -> () {
+pub fn parse_integer_literal(state: &mut State) -> char {
     match state.character {
         | None => expected("an integer literal", "end of file"),
-        | Some(character) if is_numeric(character) => get_character(state),
+        | Some(character) if is_numeric(character) => {
+            get_character(state);
+            return character;
+        }
         | Some(character) => expected("an integer literal", format!("'{character}'")),
     };
 }
